361d27c83c788f7444f734c4886d55a43ab2247b
Ali Cangül <cangulali1@gmail.com>
2023-11-01 01:37:52 +0300

    search fixed

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,9 +1,9 @@
-import { Button } from "antd";
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { LuShoppingCart } from "react-icons/lu";
+import { GrFavorite } from "react-icons/gr";
 import { searchAction } from "../../redux/actions/search";
-import {GrFavorite} from "react-icons/gr"
+
 function Navbar(props) {
   const dispatch = useDispatch();
   const { cardItems } = useSelector((state) => state.card);
@@ -12,45 +12,44 @@ function Navbar(props) {
   const [search, setSearch] = useState("");
 
   const searchPost = (e) => {
-      dispatch(searchAction(search));
-      setSearch(e.target.value)
-
+    setSearch(e.target.value); // State'i önce güncelle
+    dispatch(searchAction(search)); // Ardından searchAction'ı çağır
   };
-  return (
-    <div className=" border-solid border-2 ">
 
-      <div className="w-3/4 md:flex justify-between m-auto p-2 md:p-5 items-center">
-        <div onClick={()=>window.location="/"} className={"cursor-pointer p-2 text-center"}>
-          <h1 className={"font-bold"}>SHOPPING APP</h1>
-        </div>
-        <div className="flex content-center items-center ">
-          <div>
-            <input
-              value={search}
-              onChange={(e) => searchPost(e)}
-              placeholder="Search..."
-              className="border p-3 outline-none rounded-lg"
-            />
+  return (
+      <div className=" border-solid border-2 ">
+        <div className="w-3/4 md:flex justify-between m-auto p-2 md:p-5 items-center">
+          <div onClick={() => window.location = "/"} className={"cursor-pointer p-2 text-center"}>
+            <h1 className={"font-bold"}>SHOPPING APP</h1>
           </div>
-          <div
-            className="relative ml-2 md:ml-5"
-            onClick={() => dispatch({ type: "DRAWER", payload: true })}>
-            <LuShoppingCart size={25} className="cursor-pointer" />
-            <span className="absolute -top-2 -right-3 px-2 bg-red-600 text-white rounded-full text-sm">
+          <div className="flex content-center items-center ">
+            <div>
+              <input
+                  value={search}
+                  onChange={(e) => searchPost(e)}
+                  placeholder="Search..."
+                  className="border p-3 outline-none rounded-lg"
+              />
+            </div>
+            <div
+                className="relative ml-2 md:ml-5"
+                onClick={() => dispatch({ type: "DRAWER", payload: true })}>
+              <LuShoppingCart size={25} className="cursor-pointer" />
+              <span className="absolute -top-2 -right-3 px-2 bg-red-600 text-white rounded-full text-sm">
               {cardItems?.length}
             </span>
-          </div>
-          <div
-              className="relative ml-5"
-              onClick={() => dispatch({ type: "FAVORITE", payload: true })}>
-            <GrFavorite size={25} className="cursor-pointer" />
-            <span className="absolute -top-2 -right-3 px-2 bg-red-600 text-white rounded-full text-sm">
+            </div>
+            <div
+                className="relative ml-5"
+                onClick={() => dispatch({ type: "FAVORITE", payload: true })}>
+              <GrFavorite size={25} className="cursor-pointer" />
+              <span className="absolute -top-2 -right-3 px-2 bg-red-600 text-white rounded-full text-sm">
               {favItems?.length}
             </span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
   );
 }
 
